1fe98190f4dde988387963bed02a7d2abed079a9
ahmedwagd <ahmedwa122@gmail.com>
2021-07-31 14:05:52 +0200

    the net Ninja css variables

--- swatches.js
+++ swatches.js
@@ -4,5 +4,15 @@ var root = document.querySelector(':root');
 swatches.forEach((swatch) => {
     swatch.addEventListener('click', (e) => {
         root.style.setProperty('--theme-color', e.target.style.background);
+        console.log(e.target.style.background)
     });
 });
+// var all = document.querySelectorAll('*');
+
+// all.forEach((one) => {
+//     one.addEventListener('click', function (e) {
+//         console.log(getComputedStyle(e.target).fontSize)
+//         one.style.setProperty('font-size', '+50px')
+//         console.log(getComputedStyle(e.target).fontSize)
+//     })
+// })
